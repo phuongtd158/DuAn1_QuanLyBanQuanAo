20b60931842dec1d39455453397a5817416182cc
phuongtd158 <tdphuong2002@gmail.com>
2021-11-08 22:04:32 +0700

    add

--- src/GUI/Jfr_SanPham.java
+++ src/GUI/Jfr_SanPham.java
@@ -60,7 +60,6 @@ public class Jfr_SanPham extends javax.swing.JInternalFrame {
         cbbBoxModel = (DefaultComboBoxModel) cbbMauSac.getModel();
         cbbBoxModel.removeAllElements();
         listMS = (ArrayList<MauSac>) daoMS.selectAll();
-
         for (MauSac mauSac : listMS) {
             cbbBoxModel.addElement(mauSac);
         }
